176c14ad986b62b33f458850b9d80d35afbb05ae
royqh1979 <royqh1979@gmail.com>
2019-02-23 17:09:30 +0800

    * add more examples

---
+++ Chap06Applications/1-1.人工增长模型.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+real_n = [3.93,5.31,7.24,9.64,12.87,17.07,23.19,31.44,38.56,50.16,
+          62.95,76.00,91.99,105.71,122.76,131.67,150.70,179.32,203.21,226.51,
+            248.71,281.42] # 美国真实人口数量，用于比较
+
+N = (2000-1790)//10 # 迭代次数
+n = [0]*(N+1)
+years = [0]*(N+1)
+r = 0.2743
+n[0] = 3.93  # 1790年人口数，单位为百万
+years[0] = 1790
+for i in range(1,N+1):
+    n[i]=n[i-1]+n[i-1]*r
+    years[i]=years[i-1]+10
+plt.plot(years,n, marker='.',color='red')
+plt.plot(years,real_n, marker='.',color='blue')
+plt.show()
