3785a60285967f7ab0e68ddad5a410a87e7262e7
Roy Qu <royqh1979@gmail.com>
2019-05-28 17:03:36 +0800

    * work save

--- References/Class.Design.By.Qt/Prob.02.彩票选号小助手/sports_lottery_dialog.py
+++ References/Class.Design.By.Qt/Prob.02.彩票选号小助手/sports_lottery_dialog.py
@@ -1,7 +1,10 @@
+from time import sleep
 from typing import List
 
+from PyQt5.QtCore import QThread
+
 from sports_lottery_dlg import Ui_Dialog
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 import random
 
 
@@ -11,21 +14,30 @@ class SportsLotteryDialog(QtWidgets.QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
-        # 从GridLayout的各单元格中取出对应的文本框控件，并保存在txt_numbers和txt_special_numbers列表中
-        self.txt_numbers:List[List[QtWidgets.QTextEdit]]=[] # 5行6列，共30个文本框
-        self.txt_special_numbers:List[QtWidgets.QTextEdit]=[] # 5行1列，共1个文本框
+        # 从GridLayout的各单元格中取出对应的文本框控件，并保存在btn_numbers和btn_special_numbers列表中
+        self.btn_numbers:List[List[QtWidgets.QPushButton]]=[] # 5行6列，共30个文本框
+        self.btn_special_numbers:List[QtWidgets.QPushButton]=[] # 5行1列，共1个文本框
 
         for i in range(self.ui.grpNumbersLayout.rowCount()):
             numbers = []
             for j in range(6):
                 item = self.ui.grpNumbersLayout.itemAtPosition(i,j)
-                txt_number = item.widget()
-                txt_number.setProperty("data_row",i)
-                txt_number.setProperty("data_col",j)
-                numbers.append(txt_number)
-            self.txt_numbers.append(numbers)
+                btn_number:QtWidgets.QPushButton = item.widget()
+                btn_number.setProperty("data_row",i)
+                btn_number.setStyleSheet("QPushButton{background-color: pink; font-size:28px; "
+                                         "font-weight: bold; border: 2px groove gray; border-style:inset;}")
+                btn_number.clicked.connect(self.on_btn_number_clicked)
+                numbers.append(btn_number)
+            self.btn_numbers.append(numbers)
             item = self.ui.grpNumbersLayout.itemAtPosition(i,7)
-            self.txt_special_numbers.append(item.widget())
+            btn_special_number:QtWidgets.QPushButton = item.widget()
+            btn_special_number.setProperty("data_row",i)
+            btn_special_number.setStyleSheet(
+                "QPushButton{background-color: DarkSeaGreen; font-size:28px; font-weight: bold; "
+                "border: 2px groove gray; border-style:inset;}")
+
+            btn_special_number.clicked.connect(self.on_btn_special_number_clicked)
+            self.btn_special_numbers.append(btn_special_number)
 
         # 从VBoxLayout的各行中取出对应的单选按钮控件，并保存在radio_bets列中中
         self.radio_bets:List[QtWidgets.QRadioButton]=[]
@@ -38,16 +50,57 @@ class SportsLotteryDialog(QtWidgets.QDialog):
 
 
         self.current_row = 0
+        self.radio_bets[self.current_row].setChecked(True)
+
+        self.timer = QtCore.QTimer(self)
+        self.ui.btnStart.clicked.connect(self.on_start_gen_numbers)
+        self.ui.btnStop.clicked.connect(self.on_stop_gen_numbers)
+
+
 
     def on_bet_select(self):
         radio_bet = self.sender()
         row = radio_bet.property("data_row")
         print(row)
 
-    def on_txt_clicked(self):
-        txt_number = self.sender()
-        row = txt_number.property("data_row")
-        col = txt_number.property("data_col")
+    def on_btn_number_clicked(self):
+        btn_number : QtWidgets.QPushButton = self.sender()
+        row = btn_number.property("data_row")
+        self.radio_bets[row].setChecked(True)
+        self.current_row=row
+        gen_rand_digit(btn_number,9)
+
+    def on_btn_special_number_clicked(self):
+        btn_special_number : QtWidgets.QPushButton = self.sender()
+        row = btn_special_number.property("data_row")
+        self.radio_bets[row].setChecked(True)
+        self.current_row=row
+        gen_rand_digit(btn_special_number,4)
+
+    def on_start_gen_numbers(self):
+        self.timer.timeout.connect(self.on_generating)
+        self.timer.start(20)
+
+    def on_generating(self):
+        for i in range(len(self.btn_numbers[self.current_row])):
+            btn_number : QtWidgets.QPushButton = self.btn_numbers[self.current_row][i]
+            btn_number.setText(str(random.randint(0, 9)))
+        btn_special_number : QtWidgets.QPushButton = self.btn_special_numbers[self.current_row]
+        btn_special_number.setText(str(random.randint(0, 4)))
+
+    def on_stop_gen_numbers(self):
+        self.timer.stop()
+        self.timer.timeout.disconnect(self.on_generating)
+
+def gen_rand_digit(btn:QtWidgets.QPushButton, max_value:int):
+    for i in range(10):
+        btn.setText(str(random.randint(0, max_value)))
+        btn.repaint()
+        QThread.msleep(20)
+
+
+
+
 
 
 
