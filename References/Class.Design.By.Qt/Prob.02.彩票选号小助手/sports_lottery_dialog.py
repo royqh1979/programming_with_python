3472e9c57bd065d22e2c56d01a9d445d7bf654ee
Roy Qu <royqh1979@gmail.com>
2019-05-28 22:11:57 +0800

    * 初步完成问题2， 体育彩票

--- References/Class.Design.By.Qt/Prob.02.彩票选号小助手/sports_lottery_dialog.py
+++ References/Class.Design.By.Qt/Prob.02.彩票选号小助手/sports_lottery_dialog.py
@@ -49,21 +49,29 @@ class SportsLotteryDialog(QtWidgets.QDialog):
             self.radio_bets.append(radio_bet)
 
 
-        self.current_row = 0
+        self.current_row = 0 #当前被选中的行号
         self.radio_bets[self.current_row].setChecked(True)
 
         self.timer = QtCore.QTimer(self)
         self.ui.btnStart.clicked.connect(self.on_start_gen_numbers)
         self.ui.btnStop.clicked.connect(self.on_stop_gen_numbers)
+        self.ui.btnAutoGenerate.clicked.connect(self.on_start_gen_all_numbers)
+        self.ui.btnReturn.clicked.connect(self.close)
+        self.ui.btnStop.setEnabled(False)
 
 
 
     def on_bet_select(self):
+        """
+        处理单选钮被选中事件
+        """
         radio_bet = self.sender()
-        row = radio_bet.property("data_row")
-        print(row)
+        self.current_row = radio_bet.property("data_row")
 
     def on_btn_number_clicked(self):
+        """
+        处理单个数字框被点击事件
+        """
         btn_number : QtWidgets.QPushButton = self.sender()
         row = btn_number.property("data_row")
         self.radio_bets[row].setChecked(True)
@@ -71,26 +79,85 @@ class SportsLotteryDialog(QtWidgets.QDialog):
         gen_rand_digit(btn_number,9)
 
     def on_btn_special_number_clicked(self):
+        """
+        处理单个（特殊）数字框被点击事件
+        """
         btn_special_number : QtWidgets.QPushButton = self.sender()
         row = btn_special_number.property("data_row")
         self.radio_bets[row].setChecked(True)
         self.current_row=row
         gen_rand_digit(btn_special_number,4)
 
-    def on_start_gen_numbers(self):
-        self.timer.timeout.connect(self.on_generating)
+    def on_start_gen_all_numbers(self)->None:
+        """
+        开始逐行生成随机数字
+
+        """
+        self.ui.btnStop.setEnabled(False)
+        self.ui.btnStart.setEnabled(False)
+        self.ui.btnAutoGenerate.setEnabled(False)
+        self.timer.setProperty("data_count",0)
+        self.timer.setProperty("data_row",0)
+        self.timer.timeout.connect(self.on_generating_all)
         self.timer.start(20)
 
-    def on_generating(self):
+    def on_generating_all(self)->None:
+        """
+        定时逐行生成随机数字
+        """
+        count=self.timer.property("data_count")
+        row = self.timer.property("data_row")
+        for i in range(len(self.btn_numbers[row])):
+            btn_number : QtWidgets.QPushButton = self.btn_numbers[row][i]
+            btn_number.setText(str(random.randint(0, 9)))
+        btn_special_number : QtWidgets.QPushButton = self.btn_special_numbers[row]
+        btn_special_number.setText(str(random.randint(0, 4)))
+        count+=1
+        if count>10:
+            count=0
+            row+=1
+            print(count,row)
+            if row>=len(self.btn_special_numbers):
+                self.timer.stop()
+                self.timer.timeout.disconnect(self.on_generating_all)
+                self.ui.btnStop.setEnabled(False)
+                self.ui.btnStart.setEnabled(True)
+                self.ui.btnAutoGenerate.setEnabled(True)
+                return
+        self.timer.setProperty("data_count",count)
+        self.timer.setProperty("data_row",row)
+
+
+    def on_start_gen_numbers(self)->None:
+        """
+        开始在一行上生成随机数字（开始按钮被点击）
+        """
+        self.timer.timeout.connect(self.on_generating)
+        self.timer.start(20)
+        self.ui.btnStop.setEnabled(True)
+        self.ui.btnStart.setEnabled(False)
+        self.ui.btnAutoGenerate.setEnabled(False)
+
+    def on_generating(self)->None:
+        """
+        定时在一行上生成随机数字
+        """
         for i in range(len(self.btn_numbers[self.current_row])):
             btn_number : QtWidgets.QPushButton = self.btn_numbers[self.current_row][i]
             btn_number.setText(str(random.randint(0, 9)))
         btn_special_number : QtWidgets.QPushButton = self.btn_special_numbers[self.current_row]
         btn_special_number.setText(str(random.randint(0, 4)))
 
-    def on_stop_gen_numbers(self):
+    def on_stop_gen_numbers(self)->None:
+        """
+        停止在一行上生成随机数字（停止按钮被点击）
+        """
         self.timer.stop()
         self.timer.timeout.disconnect(self.on_generating)
+        self.ui.btnStop.setEnabled(False)
+        self.ui.btnStart.setEnabled(True)
+        self.ui.btnAutoGenerate.setEnabled(True)
+
 
 def gen_rand_digit(btn:QtWidgets.QPushButton, max_value:int):
     for i in range(10):
