ab56cbfff9c910d6e36252ee9fe687bdf841c1e0
Roy Qu <royqh1979@gmail.com>
2019-06-06 15:50:40 +0800

    * 增加另存word为pdf功能  * 增加合并pdf为一个文件的功能

--- Chap05Numpy/3-2.pandas应用.py
+++ Chap05Numpy/3-2.pandas应用.py
@@ -7,9 +7,8 @@ filename = dlg.get_open_file_name("请选择要打开的csv文件",dlg.FileFilte
 if filename =="":
     print("未选择文件")
     exit(-1)
-with open(filename,mode="r",encoding="GBK") as file:
-    df=pd.read_csv(file)
+df=pd.read_csv(filename,encoding='GBK')
 df["销售额"]=df["单价"]*df["数量"]
 index=df["销售额"]>100 #生成筛选bool数组
 new_df=df[index] #使用bool数组筛选行
-new_df.to_csv("result.csv")
+new_df.to_csv("result.csv",encoding='UTF-8')
