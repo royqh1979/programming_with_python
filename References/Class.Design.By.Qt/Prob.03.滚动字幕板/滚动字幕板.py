441f1d7a7fc8ac14d002c56acf6a4aa0a1817b39
Roy Qu <royqh1979@gmail.com>
2019-06-10 08:40:58 +0800

    * 完成新例子

--- References/Class.Design.By.Qt/Prob.03.滚动字幕板/滚动字幕板.py
+++ References/Class.Design.By.Qt/Prob.03.滚动字幕板/滚动字幕板.py
@@ -5,21 +5,25 @@ from display_window import Ui_Dialog as DisplayWindowUi
 from control_panel import Ui_Dialog as ControlPanelUi
 from PyQt5 import QtWidgets, QtCore, QtGui
 
+import random
+
 
 @dataclass
 class Color:
     name:str
-    value:QtGui.QColor
+    value:str
+    contrast_color: str
 
 colors = [
-    Color('黑色','black'),
-    Color('绿色', 'green'),
-    Color('蓝色', 'blue'),
-    Color('青色', 'cyan'),
-    Color('红色', 'red'),
-    Color('黄色', 'yellow'),
-    Color('紫色', 'purple'),
+    Color('黑色','black','white'),
+    Color('绿色', 'green','purple'),
+    Color('蓝色', 'blue','yellow'),
+    Color('青色', 'cyan','red'),
+    Color('红色', 'red','cyan'),
+    Color('黄色', 'yellow','blue'),
+    Color('紫色', 'purple','cyan'),
 ]
+
 
 font_names = [
     "宋体",
@@ -43,6 +47,7 @@ class DisplayWindow(QtWidgets.QDialog):
         super().__init__();
         self.ui=DisplayWindowUi()
         self.ui.setupUi(self)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
         app.quit()
@@ -81,52 +86,128 @@ class ControlPanel(QtWidgets.QDialog):
         self._timer = QtCore.QTimer()
         self._timer.timeout.connect(self.on_time_out)
         self.ui.btnStart.clicked.connect(self.toggle_rolling)
+        self.ui.txtCustom.returnPressed.connect(self.clear_txt_selection)
+
+        self.ui.sldSpeed.setRange(1,20)
+        self.ui.sldSpeed.valueChanged.connect(self.set_speed)
+        self.ui.sldSpeed.setValue(1)
+        self._speed=1
+        self._orient_left = True
+
+        self.ui.cbChangeColor.stateChanged.connect(self.set_change_color)
+        self._frame_count = 0
+
+    def set_change_color(self):
+        #如果打开“自动改变颜色”，则禁用前景色和背景色选择
+        enable_color = not self.ui.cbChangeColor.isChecked()
+        self.ui.cbBackground.setEnabled(enable_color)
+        self.ui.cbForeground.setEnabled(enable_color)
+
+    def set_speed(self,speed):
+        self._speed = speed
+
+    def clear_txt_selection(self):
+        for btn in self.txt_btns:
+            btn.setChecked(False)
 
     def on_time_out(self):
-        if self._rolling:
-            lblDisplay = self.display_window.ui.lblDisplay
-            lblDisplay.move(lblDisplay.x()-1,lblDisplay.y())
+        if not self._rolling:
+            return
+        lblDisplay = self.display_window.ui.lblDisplay
+        x=lblDisplay.x()
+        y=lblDisplay.y()
+        if self.ui.rdOrientLeft.isChecked():
+            self._orient_left = True
+            if lblDisplay.x()+lblDisplay.width() < 0:
+                x=self.display_window.width()
+            else:
+                x=lblDisplay.x()-self._speed
+        elif self.ui.rdOrientRight.isChecked():
+            self._orient_left = False
+            if lblDisplay.x() > self.display_window.width():
+                x=-lblDisplay.width()
+            else:
+                x=lblDisplay.x()+self._speed
+        elif self.ui.rdSwing.isChecked():
+            if self._orient_left:
+                if lblDisplay.x() + lblDisplay.width() < 0:
+                    self._orient_left = False
+                else:
+                    x = lblDisplay.x() - self._speed
+            else:
+                if lblDisplay.x() > self.display_window.width():
+                    self._orient_left = True
+                else:
+                    x = lblDisplay.x() + self._speed
+        else:
+            if self._orient_left:
+                if lblDisplay.x() <= 0:
+                    self._orient_left = False
+                else:
+                    x = lblDisplay.x() - self._speed
+            else:
+                if lblDisplay.x()+lblDisplay.width() >= self.display_window.width():
+                    self._orient_left = True
+                else:
+                    x = lblDisplay.x() + self._speed
+
+        lblDisplay.move(x,y)
+        if self._frame_count==0:
+            if self.ui.cbChangeColor.isChecked():
+                color = random.choice(colors)
+                lblDisplay.setStyleSheet(f"QLabel {{ color: {color.value}; background-color: {color.contrast_color}; }}")
+                self.display_window.setStyleSheet(f"QWidget {{background-color: {color.contrast_color}; }}")
+        self._frame_count += 1
+        if self._frame_count > 30:
+            self._frame_count = 0
+
 
     def toggle_rolling(self):
         if self._rolling:
             self._rolling = False
             self._timer.stop()
+            self.ui.btnStart.setText("开始滚动")
         else:
             self._rolling = True
+            self.ui.btnStart.setText("停止滚动")
             lblDisplay = self.display_window.ui.lblDisplay
             font = lblDisplay.font()
 
+            # 字体大小
             if self.ui.rdBigFont.isChecked():
                 font.setPixelSize(40)
             else:
                 font.setPixelSize(28)
 
+            #字体名称
             for i in range(len(font_names)):
                 if self.font_name_btns[i].isChecked():
                     font.setFamily(font_names[i])
 
             lblDisplay.setFont(font)
+            # 文字内容
             for i in range(len(txts)):
                 if self.txt_btns[i].isChecked():
                     lblDisplay.setText(txts[i])
                     break
             else:
                 lblDisplay.setText(self.ui.txtCustom.text())
-            fore_color = self.ui.cbForeground.itemData(self.ui.cbForeground.currentIndex())
-            back_color = self.ui.cbBackground.itemData(self.ui.cbBackground.currentIndex())
 
-            print(fore_color,back_color)
+            # 文字颜色
+            if not self.ui.cbChangeColor.isChecked():
+                fore_color = self.ui.cbForeground.itemData(self.ui.cbForeground.currentIndex())
+                back_color = self.ui.cbBackground.itemData(self.ui.cbBackground.currentIndex())
+                lblDisplay.setStyleSheet(f"QLabel {{ color: {fore_color}; background-color: {back_color}; }}")
+                self.display_window.setStyleSheet(f"QWidget {{background-color: {back_color}; }}")
 
-            lblDisplay.setStyleSheet(f"QLabel {{ color: {fore_color}; background-color: {back_color}; }}")
-            self.display_window.setStyleSheet(f"QWidget {{background-color: {back_color}; }}")
+            # 重新设置滚动标签大小（以便完整显示所有文字）
             size=lblDisplay.sizeHint()
             lblDisplay.setMinimumSize(size.width(),size.height())
             self._timer.start(50)
 
 
-
-
 if __name__=='__main__':
+    random.seed()
     app = QtWidgets.QApplication([])
     display_window = DisplayWindow()
     control_panel = ControlPanel(display_window)
