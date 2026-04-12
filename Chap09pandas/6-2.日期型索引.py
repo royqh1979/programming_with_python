ef7dbefe191361ddb1a7bae05a5fdb3b6e3d32ef
Roy Qu <royqh1979@gmail.com>
2026-04-12 13:16:35 +0800

    增加pandas相关示例

---
+++ Chap09pandas/6-2.日期型索引.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("bank_stock.csv",
+                 encoding="GBK",
+                 index_col=0)
+#转化为日期型
+df.index = pd.to_datetime(df.index)
+#计算年月日
+df["年"]=df.index.year
+df["月"]=df.index.month
+df["日"]=df.index.day
+print(df)
+#计算相差天数
+diff = df.index-pd.to_datetime("2001-01-01")
+print(diff.days)
+#按月统计
+print(df["收盘价"].resample("ME").mean())
