d6bc1da08d58029f68f9c5b9c161bf57acedf953
Roy Qu <royqh1979@gmail.com>
2026-04-25 17:39:25 +0800

    泊松回归案例增加发生率比IRR计算 更换泊松回归示例数据文件

--- Chap10StatisticalAnalysis/6-3.泊松回归.py
+++ Chap10StatisticalAnalysis/6-3.泊松回归.py
@@ -1,9 +1,22 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv("medpar.csv")
+df = pd.read_csv("defection-detect.csv")
+
+df = df.rename(columns={"Discoloration Defects":"Discoloration_Defects",
+           "Hours Since Cleanse":"Hours_Since_Cleanse",
+           "Size of Screw":"Size_of_Screw",
+           "Clump Defects":"Clump_Defects"
+           })
+print(np.mean(df["Discoloration_Defects"]))
+print(np.var(df["Discoloration_Defects"]))
+print(df)
 
 import statsmodels.formula.api as sm
-
-model = sm.poisson("los ~ hmo + white + type2 + type3", data=df)
+model = sm.poisson("Discoloration_Defects ~ Hours_Since_Cleanse + Size_of_Screw + Temperature + Clump_Defects", data=df)
 lm = model.fit()
 print(lm.summary())
+
+print(" ---- incident rate ratio ---- ")
+import numpy as np
+print(np.exp(lm.params))
