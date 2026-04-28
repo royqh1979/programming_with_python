234337713d325eddca357717e0be36cd48db0864
Roy Qu <royqh1979@gmail.com>
2026-04-28 18:09:08 +0800

    增加卡方检验相关示例

---
+++ Chap10StatisticalAnalysis/2-4-2.卡方拟合优度检验.py
@@ -0,0 +1,3 @@
+import scipy.stats as stats
+
+print(stats.chisquare([50,50],[70,30],ddof=1))
---
+++ Chap10StatisticalAnalysis/2-4-3.卡方独立性检验.py
@@ -0,0 +1,3 @@
+import scipy.stats as stats
+
+print(stats.chisquare([43,44,9,4],[45.24,41.76,6.76,6.24],ddof=1))
