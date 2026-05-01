b9457b7e5caa7f5f8a3d09aa01bcd106cd676941
Roy Qu <royqh1979@gmail.com>
2026-05-01 09:14:55 +0800

    增加缺失值相关示例

--- Chap13FeatureEngineering/1 缺失值表示.py
+++ Chap13FeatureEngineering/1 缺失值表示.py
@@ -2,4 +2,11 @@ import pandas as pd
 
 df=pd.read_csv("missing-values.csv")
 print(df)
-print(df["Sex"])
+print(" -- DataFrame.isna() --")
+print(df.isna())
+print(" -- Series.isna() --")
+print(df["Math"].isna())
+print(" -- 找出哪些列中有缺失值 -- ")
+print(df.isna().any())
+print(" -- 找出哪些行中有缺失值 -- ")
+print(df.isna().any(axis=1))
